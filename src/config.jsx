612e13494569c77cb886e2accc10fa42fb52070e
mdnriyas17 <mdnriyas@gmail.com>
2023-09-13 10:07:31 +0530

    change

--- src/config.jsx
+++ src/config.jsx
@@ -1,8 +1,3 @@
 export const config = {
-<<<<<<< HEAD
     api: 'https://guvi-hackathon2-backend-beige.vercel.app'
-=======
- api: 'https://guvi-hackathon2-backend-beige.vercel.app'
-
->>>>>>> 1461f4db22d87dd8d7737b47e7160c8b72975ac5
 }
